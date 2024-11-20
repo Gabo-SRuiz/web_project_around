41143544fce6dda842dd4fc0797a54bd0eb0c5b3
Gabriel <gabo4534@gmail.com>
2024-11-19 19:00:45 -0600

    Segundo commit

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+let editButton = document.querySelector(".profile__editbutton");
+let popup = document.querySelector(".popup__container")
+let closeButton = document.querySelector(".popup__close-button");
+let form = document.querySelector(".popup");
+let inputName = document.querySelector(".popup__input-name");
+let inputDescription = document.querySelector(".popup__input-description");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let saveButton = document.querySelector(".popup__button")
+
+function toggleForm() {
+    popup.classList.toggle("popup__visible");
+}
+
+editButton.addEventListener('click', toggleForm);
+closeButton.addEventListener("click", toggleForm);
+saveButton.addEventListener("click", toggleForm);
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputDescription.value;
+
+}
+
+form.addEventListener("submit", handleFormSubmit);
+
+
+function abledButton() {
+    let validName = inputName.value.trim() !=="";
+    let validDescription = inputDescription.value.trim() !=="";
+
+    if (validName || validDescription) {
+        saveButton.removeAttribute("disabled");
+        saveButton.classList.add("popup__save-button");
+    } else {
+        saveButton.setAttribute("disabled", true);
+        saveButton.classList.remove("popup__save-button");
+    }
+}
+
+inputName.addEventListener("input", abledButton);
+inputDescription.addEventListener("input", abledButton);
+
