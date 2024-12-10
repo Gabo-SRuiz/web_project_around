5c5e86fedb09f93db074fc3f97a3fb8018975929
Gabriel <gabo4534@gmail.com>
2024-12-09 19:30:55 -0600

    Proyecto 8 TripleTen primer commit

--- README.md
+++ README.md
@@ -18,5 +18,31 @@ Los comandos son todas las herramientas que nos ayudan para darle a las variable
 
 Las funciones son conjuntos de comandos que se utilizan para darle funciones a las variables u objetos y estas se utilizan cuando se las llama.
 
+//////////////////////////////////////////////////////////////////////////////////
+
+Proyecto del sprint 8 de TripleTen
+
+Seguimos trabajando con el mismo repositorio y la misma pagina que el proyecto aneterior. Ahora las herramientas implementadas y los metodos utilizados fueron exclusivos de javaScript sin contar lo que ya se sabia de html. Se añadio lo siguiente:
+
+    - Nuevo formulario para añadir tarjetas con la imagen y el titulo personalizados.
+
+    - Tarjetas iniciales sacadas de un array proporcionado por el mismo sprint.
+
+    - Boton de "me gusta"
+
+    - Boton de basura (para eliminar las tarjetas).
+
+    -Nuevo popup con la imagen y el titulo de la tarjeta.
+
+Las tarjetas fueron eliminadas de html inicial y se incluyeron en base al array con el metodo para clonar un <template> "cloneNode".
+
+Las tarjetas que se añadian mediante el formulario utilice el mismo <template> que el utilizado para las tarjetas iniciales.
+
+El boton me gusta fue implementado cambiandole una clase con "classList.toggle"
+
+Utilizando ".remove()" para deshacernos de las tarjetas.
+
+El popup fue implementado sustituyendo en la funcion los valores de las tarjetas para darsela al mismo.
+
 Enlace del proyecto:
 https://gabo-sruiz.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,146 @@
-let editButton = document.querySelector(".profile__editbutton");
-let popup = document.querySelector(".popup")
-let closeButton = document.querySelector(".popup__close-button");
-let form = document.querySelector(".popup__container");
-let inputName = document.querySelector(".popup__input-name");
-let inputDescription = document.querySelector(".popup__input-description");
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
-let saveButton = document.querySelector(".popup__button")
+const editButton = document.querySelector(".profile__editbutton");
+const popup = document.querySelector(".popup")
+const closeButton = document.querySelector(".popup__close-button");
+const form = document.querySelector(".popup__container");
+const inputName = document.querySelector(".popup__input-name");
+const inputDescription = document.querySelector(".popup__input-description");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const saveButton = document.querySelector(".popup__button");
+const addButton = document.querySelector(".profile__new-picture");
+const cancelButton = document.querySelector(".form__xbutton");
+const confirmButton = document.querySelector(".form__button");
+const cardForm = document.querySelector(".form__container");
+const pictures = document.querySelector(".pictures");
+const cardName = document.querySelector(".form__input-name");
+const cardLink = document.querySelector(".form__input-link"); 
+const initialCards = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+    },
+    {
+        name: "Lago Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+        name: "Montañas Calvas",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ];
+  
+  function createCardElements(name, link) {
+      const cardsTemplate = document.querySelector(".pictures__template").content;
+      const cards = cardsTemplate.querySelector(".pictures__item").cloneNode(true);
+      const cardTitle = cards.querySelector(".pictures__title");
+      const cardImage = cards.querySelector(".pictures__image");
+      const likeButton = cards.querySelector(".pictures__button");
+      const deleteButton = cards.querySelector(".pictures__delete-button");
+      
+      likeButton.addEventListener("click", function(evt) {
+        evt.target.classList.toggle("pictures__button-active")
+      });
+
+      deleteButton.addEventListener("click", function(){
+        cards.remove();
+      });
+
+      cardImage.addEventListener("click", function() {
+        const imagePopup = document.querySelector(".images__content");
+        const imageTitle = document.querySelector(".images__paragraph");
+        const closeImageButton = document.querySelector(".images__close-button");
+        const images = document.querySelector(".images");
+
+        images.classList.toggle("images__visible");
+        
+        imagePopup.src = cardImage.src;
+        imageTitle.textContent = cardTitle.textContent;
+
+        closeImageButton.addEventListener("click", function() {
+            images.classList.remove("images__visible");
+        });
+      });
+
+    cardTitle.textContent = name;
+    cardImage.src = link;
+
+    return cards;
+
+  }
+
+  initialCards.forEach(function (item) {
+    const cardElement = createCardElements(item.name, item.link);
+    pictures.append(cardElement);
+
+});
+
+function createCard(evt) {
+    evt.preventDefault();
+    const cardElement = createCardElements(cardName.value, cardLink.value);
+    pictures.prepend(cardElement);
+
+    cardForm.reset();
+}
+
+    cardForm.addEventListener("submit", createCard);
+
+function formFieldChecker() {
+    const validName = cardName.value.trim() !=="";
+    const validLink = cardLink.value.trim() !=="";
+
+    if (validName || validLink) {
+        confirmButton.removeAttribute("disabled");
+        confirmButton.classList.add("form__save-button");
+    } else {
+        confirmButton.setAttribute("disabled", true);
+        confirmButton.classList.remove("form__save-button");
+    }
+}
+
+cardName.addEventListener("input", formFieldChecker);
+cardLink.addEventListener("input", formFieldChecker);
 
 function toggleForm() {
     popup.classList.toggle("popup__visible");
-}
+} 
+
 
 editButton.addEventListener('click', toggleForm);
 closeButton.addEventListener("click", toggleForm);
 saveButton.addEventListener("click", toggleForm);
 
-function handleFormSubmit(event) {
+function showCardForm() {
+    cardForm.classList.toggle("form__visible");
+} 
+
+addButton.addEventListener("click", showCardForm);
+cancelButton.addEventListener("click", showCardForm);
+confirmButton.addEventListener("click", showCardForm);
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputDescription.value;
 
-}
+    form.reset();
 
-form.addEventListener("submit", handleFormSubmit);
-
+});
 
 function fieldChecker() {
-    let validName = inputName.value.trim() !=="";
-    let validDescription = inputDescription.value.trim() !=="";
+    const validName = inputName.value.trim() !=="";
+    const validDescription = inputDescription.value.trim() !=="";
 
     if (validName || validDescription) {
         saveButton.removeAttribute("disabled");
@@ -41,4 +153,3 @@ function fieldChecker() {
 
 inputName.addEventListener("input", fieldChecker);
 inputDescription.addEventListener("input", fieldChecker);
-
